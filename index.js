699509acecfeacb0b8b9a7223d279e33dcd62f64
Manan Lalwani <112713667+mananlalwani@users.noreply.github.com>
2023-10-27 13:48:53 -0500

    added basic backend and fixed some js/css stuff
    Need to fix backend
    
    Co-Authored-By: Denial256 <149196472+Denial256@users.noreply.github.com>
    Co-Authored-By: nulllentity-exe <129435100+nulllentity-exe@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-$("#StartScreen").slideDown();
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    }
+);
---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+//slides the title screen down
+$("StartScreen").slideDown();
+//Make all the screens/divs slide up at the start of the game
+$("#MyBlobs").slideUp();
+$("#ExtraStuff").slideUp();
+$("#GetBlobs").slideUp();
+$("#settings").slideUp();
+$("#slimepedia").slideUp();
+$("#ExtraCode").slideUp();
+$("#DevHacks").slideUp();
+$("#Blobs").slideUp();
+$("#Shop").slideUp();
+$("#Blobs").slideUp();
+$("#PlayScreen").slideUp();
+$("#inventory").slideUp();
+//Hide dev hacks
+$("#DevHacks").hide();
+
