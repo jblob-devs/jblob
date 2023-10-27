d1b2466e3dc3c2e8f28797b0e54d46996440ead1
Manan Lalwani <112713667+mananlalwani@users.noreply.github.com>
2023-10-27 17:09:16 -0500

    Fixed all of the backend stuff
    Should all be working now.. hopefully
    
    Also some node stuff was fixed
    
    I'll make start instructions in the next commit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const port = 3000;
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.enable("trust proxy");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index.html');
-    }
-);
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-    }
-);
+  console.log(`Example app listening at http://localhost:${port}`);
+});
