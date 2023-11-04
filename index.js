06b37a3659b60a478c65e9583ed950c789a46d27
Manan Lalwani <112713667+mananlalwani@users.noreply.github.com>
2023-11-03 22:42:36 -0500

    Start backend save system and clean some syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const notifier = require('node-notifier');
+const notifier = require("node-notifier");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const sqlite3 = require("sqlite3").verbose();
@@ -17,83 +17,101 @@ app.use(express.static("public"));
 sqlite3.verbose();
 
 db.serialize(() => {
-    db.run(
-        "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, PRIMARY KEY(username))"
-    );
-    }
-);
+  db.run("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, save TEXT)");
+});
 
 app.get("/login", (req, res) => {
-    res.render("login.html");
-    });
+  res.render("login.html");
+});
 
 app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    db.all(
-        "SELECT * FROM users WHERE username = ? AND password = ?",
-        [username, password],
-        (err, rows) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  db.all("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else if (rows.length > 0) {
+      res.cookie("username", username);
+      db.all("SELECT save FROM users WHERE username = ?", [username], (err, rows) => {
         if (err) {
-            console.log(err);
-        } else if (rows.length > 0) {
-            res.cookie("username", username);
-            res.redirect("/");
+          console.log(err);
         } else {
-            res.redirect("/login");
+          res.cookie("save", rows[0].save);
         }
-        }
-    );
+      });
+      res.redirect("/");
+    } else {
+      res.redirect("/login");
     }
-);
+  });
+});
 
 app.get("/register", (req, res) => {
-    res.render("register.html");
-}
-);
+  res.render("register.html");
+});
 
 app.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const password2 = req.body.password2;
-    if (password !== password2) {
-        res.redirect("/register");
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+  if (password !== password2) {
+    res.redirect("/register");
+  }
+  db.all("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/login");
     }
-    db.all(
-        "INSERT INTO users (username, password) VALUES (?, ?)",
-        [username, password],
-        (err, rows) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.redirect("/login");
-        }
-        }
-    );
-    }
-);
+  });
+});
+
+
 
 app.get("/logout", (req, res) => {
-    res.clearCookie("username");
-    res.redirect("/");
-}
-);
+  res.clearCookie("username");
+  res.redirect("/");
+});
 
 app.post("/code", (req, res) => {
-    const code = req.body.code;
-    if (code === "1234") {
-        res.send('<script>alert("Correct code! Great prizes will be delivered soon!")</script>')
+  const code = req.body.code;
+  if (code === "1234") {
+    res.send('<script>alert("Correct code! Great prizes will be delivered soon!")</script>');
+  } else {
+    res.send('<script>alert("Incorrect code!")</script>');
+  }
+});
+
+app.post("/save", (req, res) => {
+  const username = req.cookies.username;
+  const save = req.body.save;
+  db.all("UPDATE users SET save = ? WHERE username = ?", [save, username], (err, rows) => {
+    if (err) {
+      console.log(err);
     } else {
-        res.send('<script>alert("Incorrect code!")</script>');
+      res.redirect("/");
     }
+  });
+});
+
+app.post("/load", (req, res) => {
+  const username = req.cookies.username;
+  db.all("SELECT save FROM users WHERE username = ?", [username], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(rows[0].save);
+    }
+  });
 });
 
 app.get("/", (req, res) => {
-    if (req.cookies.username) {
-        res.render("index.html", { username: req.cookies.username });
-    } else {
-        res.render("landing.html");
-    }
+  if (req.cookies.username) {
+    res.cookie("save", req.cookies.save);
+    res.render("index.html", { username: req.cookies.username });
+  } else {
+    res.render("landing.html");
+  }
 });
 
 app.listen(port, () => {
---
+++ public/js/variables.js
@@ -0,0 +1,24 @@
+var gold = 0;
+let save = {
+    username: "",
+    save: "",
+    gold: 0,
+    blobsUnlocked: [],
+    blobTeam: [],
+
+    //Blobs
+    basicBlob: {
+        level: 1,
+        xp: 0,
+        xpNeeded: 100,
+        health: 10,
+        attack: 1,
+        defense: 1,
+        speed: 1,
+        special: 1,
+        type: "basicBlob"
+    }    
+};
+let saveString = JSON.stringify(save);
+    
+    
